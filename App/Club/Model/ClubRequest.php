1c27a830038a32240e13e750113bb2b79c6735d2
Damian Zamęcki <damian-zamecki@wp.pl>
2021-05-16 14:29:04 +0200

    Dodane modele i requesty do Klubu i Ligi

---
+++ App/Club/Model/ClubRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\User\Model;
+
+
+class ClubRequest
+{
+    private $name;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     * @return mixed
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+}
---
+++ App/League/Model/LeagueModel.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\League\Model;
+class LeagueModel
+{
+    private $id;
+    private $name;
+    private $nationality_id;
+    private $created_at;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNationalityId()
+    {
+        return $this->nationality_id;
+    }
+
+    /**
+     * @param mixed $nationality_id
+     */
+    public function setNationalityId($nationality_id)
+    {
+        $this->nationality_id = $nationality_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param mixed $created_at
+     */
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+
+}
